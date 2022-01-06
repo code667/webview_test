6673052ebd485e298670f72bc3f289f2c25bb245
code667_8964天安门事件 <39250038+code667@users.noreply.github.com>
2022-01-06 08:52:15 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webview_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        async Task InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        async void InitBrowser()
+        {
+            await InitializeAsync();
+            webView21.CoreWebView2.Navigate("https://www.html5test.com");
+        }
+    }
+}
